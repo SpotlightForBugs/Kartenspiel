577b71ae4863b859ec1ff398029d1317ac6f1711
Johannes Häusler <mail@spotlightforbugs.eu>
2023-03-27 18:15:50 +0200

    Dokumentation

--- src/main/java/eu/spotlightforbugs/Main.java
+++ src/main/java/eu/spotlightforbugs/Main.java
@@ -2,6 +2,9 @@ package eu.spotlightforbugs;
 
 public class Main {
     public static void main(String[] args) {
+        //erstellen eines Kartenspiels
+        Kartenspiel k = new Kartenspiel();
+        k.aussortieren("Herz");
 
 
 
